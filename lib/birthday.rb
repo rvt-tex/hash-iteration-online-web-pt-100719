efa1f312ec675939841148a8b371138abdd16c44
Ruel Tingling <rvtingling@gmail.com>
2019-10-24 17:38:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -11,13 +11,12 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_kids, age)
+#def age_appropriate_birthday(birthday_kids, age)
   #only prints the birthday greeting if the birthday kid 12 or younger"
-birthday_kids.each do |kids_name, age|
-  if birthday_kids == :kids_name && age < 12 
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end 
-age_appropriate_birthday
-end 
-end
+#birthday_kids.each do |kids_name, age|
+  #if birthday_kids == :kids_name && age < 12 
+    #puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+  #end
+#end 
+#age_appropriate_birthday
+#end 
